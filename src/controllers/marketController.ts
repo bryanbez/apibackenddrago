e06cbfb33b98343b824f09ba1e590614407e70c7
bryanbez <sardobryan@mgial.com>
2025-06-01 01:44:21 +0800

    update on marketController

--- src/controllers/marketController.ts
+++ src/controllers/marketController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 export default class MarketDataController {
   getDSTMarketPrice = async (req: Request, res: Response): Promise<void> => {
+    console.log("API KEY:", process.env.COINGECKO_API);
     try {
       const response = await fetch(
         "https://api.coingecko.com/api/v3/coins/dragon-soul-token",
@@ -9,14 +10,21 @@ export default class MarketDataController {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            "x-cg-demo-api-key": `${process.env.COINGECKO_API}`,
+            "x-cg-demo-api-key": process.env.COINGECKO_API || "",
           },
         }
       );
 
+      if (!response.ok) {
+        const text = await response.text();
+        console.error(`Failed with status ${response.status}`, text);
+        res.status(response.status).send(text);
+        return;
+      }
+
       const data = await response.json();
       const marketData = data.market_data.current_price.usd;
-      res.status(200).json(marketData);
+      res.status(200).json({ usd: marketData });
     } catch (error) {
       console.error("Error fetching market price:", error);
       res.status(500).json({ message: "Error fetching market price", error });
