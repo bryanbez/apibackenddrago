b468e1005cad53e225b804bcba382bf4643f1aad
bryanbez <sardobryan@mgial.com>
2025-06-02 09:28:33 +0800

    update on server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,6 @@ const PORT = process.env.PORT || 5000;
 connectToMongoDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(process.env.COINGECKO_API);
   });
 });
