8d993bd6da7364f32f914977c05e275fe5176b39
bryanbez <sardobryan@mgial.com>
2025-06-02 10:03:01 +0800

    Change market api

--- src/controllers/marketController.ts
+++ src/controllers/marketController.ts
@@ -4,14 +4,7 @@ export default class MarketDataController {
   getDSTMarketPrice = async (req: Request, res: Response): Promise<void> => {
     try {
       const response = await fetch(
-        "https://api.coingecko.com/api/v3/coins/dragon-soul-token",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "x-cg-demo-api-key": process.env.COINGECKO_API || "",
-          },
-        }
+        "https://api.geckoterminal.com/api/v2/networks/polygon_pos/pools/0x9ac431aa4a30f8881d01ea0ab648208aa5b842d2"
       );
 
       if (!response.ok) {
@@ -22,7 +15,8 @@ export default class MarketDataController {
       }
 
       const data = await response.json();
-      const marketData = data.market_data.current_price.usd;
+      const marketData = data.data.attributes.base_token_price_usd;
+      console.log("USD Price: " + marketData);
       res.status(200).json({ usd: marketData });
     } catch (error) {
       console.error("Error fetching market price:", error);
